c81b9ce2fc1829b320508bf659e7f062617a5b40
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-04-15 23:06:51 -0600

    Working on it

---
+++ ecommerce/src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-unused-vars */
+import React from 'react'; 
+import  ReactDOM  from 'react-dom';
+
+ReactDOM.render(<App />, document.getElementById('root') ); 
