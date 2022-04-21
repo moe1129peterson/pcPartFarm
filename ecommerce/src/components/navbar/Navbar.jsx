e498800d7e3aff27e4056ce87a250b2df14549ac
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-04-20 22:23:04 -0600

    assets

--- ecommerce/src/components/navbar/Navbar.jsx
+++ ecommerce/src/components/navbar/Navbar.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 import React from 'react';
+import {RiMenu3line, RiCloseLin} from 'react-icons/ri'
+import logo from '../../assets/logo.png'
+import hero from '../../assets/hero.png'
 import './navbar.css'; 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='pcf_navbar'>
+      <div className='pcf_navbar-links'>
+        <div className='pcf_navbar-links_logo'>
+          <img src={logo} alt='logo' />
+
+        </div>
+
+      </div>
+    
+    </div>
   )
 }
 
