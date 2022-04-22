304f3b46d2074b0c67eb89b692c289200ca68897
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-04-21 23:51:53 -0600

    worked on some navbar

--- ecommerce/src/components/navbar/Navbar.jsx
+++ ecommerce/src/components/navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 import React from 'react';
@@ -12,8 +14,12 @@ const Navbar = () => {
       <div className='pcf_navbar-links'>
         <div className='pcf_navbar-links_logo'>
           <img src={logo} alt='logo' />
-
         </div>
+        <div className='pcf_navbar-links_container'></div>
+        <p><a here="#home">Home</a></p>
+        <p><a here="#aboutUs">About Us</a></p>
+        <p><a here="#blog">Blog</a></p>
+
 
       </div>
     
