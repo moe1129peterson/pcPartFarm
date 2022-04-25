cce0f32e64e08cf944256c5fe48aaf0dddf2f3a9
moe1129peterson <moe1129tsuzuki@gmail.com>
2022-04-24 23:29:43 -0600

    figured out path

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import [ BrowserRouter as Router, Route, Switch] from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import {Footer, Blog, Possibility, Feature, Header} from './containers';
 import {CTA, Brand, Navbar} from './components';
@@ -12,17 +13,24 @@ import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
-        <Cpu />
+    <Router>
+      <div className="App">
+        <div className="gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <div className='content'>
+          <Switch>
+            <Route exact path='/'>
+              <Blog />
+            </Route >
+            <Route path='/cpu'>
+              <Cpu />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <Blog />
-      <Footer />
-    
-
-    </div>
+    </Router>
   )
 }
 
--- ecommerce/src/pages/cpu/Cpu.jsx
+++ ecommerce/src/pages/cpu/Cpu.jsx
@@ -8,26 +8,21 @@ import ryzen95950 from '../../assets/ryzen9-5950.jpg';
 
 const Cpu = () => {
     return (
-      <>
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
+      <div className='products'>
+        <div className='pcf_product-cpu1'>
+          <img src={ryzen5} alt='ryzen5-5600' />
+        </div>
+        <div className='pcf_product-cpu2'>
+          <img src={ryzen7} alt='ryzen5-5600' />
+        </div>
+        <div className='pcf_product-cpu3'>
+          <img src={ryzen9} alt='ryzen5-5600' />
+        </div>
+        <div className='pcf_product-cpu4'>
+          <img src={ryzen95950} alt='ryzen5-5600' />
+        </div>
       </div>
-
-      <div className='pcf_product-cpu1'>
-        <img src={ryzen5} alt='ryzen5-5600' />
-      </div>
-      <div className='pcf_product-cpu2'>
-        <img src={ryzen7} alt='ryzen5-5600' />
-      </div>
-      <div className='pcf_product-cpu3'>
-        <img src={ryzen9} alt='ryzen5-5600' />
-      </div>
-      <div className='pcf_product-cpu4'>
-        <img src={ryzen95950} alt='ryzen5-5600' />
-      </div>
-      </>
-    )
+    );
   }
 
 
